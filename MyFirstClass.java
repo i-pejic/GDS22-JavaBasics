d6028014995f0b6bbb19ba28e6cb072b659ddd8b
i-pejic <s52456@edu.campus02.at>
2022-09-23 17:10:26 +0200

    My First Class created;args as input parameter written to console

---
+++ MyFirstClass.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+    //Erste Klasse erzeugt
+    //psvm + tab
+
+    public static void main(String[] args) {
+        //Erste Ausgabe
+        System.out.println("Ich werde auf der Konsole angezeigt");
+
+        //args
+        //Kürzel sout + tab
+        System.out.println(Arrays.toString(args));
+    }
+}
